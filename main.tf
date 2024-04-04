8ad42291f25be73098abeb857af2cd4081dd3646
Александар Симић <a@repl.ist>
2024-04-04 16:17:28 +0100

    Indentation fix

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   }
 }
 
-  resource "local_file""hello_local_file" {
-    content = "Hello terraform local!"
-    filename = "${path.module}/hello_local_file.txt"
-  }
+resource "local_file""hello_local_file" {
+  content = "Hello terraform local!"
+  filename = "${path.module}/hello_local_file.txt"
+}
