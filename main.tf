b6aca299a2dcc06f7ac26e61379cfad802ddef33
Александар Симић <a@repl.ist>
2024-04-04 16:00:58 +0100

    Going with tfstate.dev setup for state

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 terraform {
-  encryption {
-    state {
-      enforced = true
-    }
-    plan {
-      enforced = true
-    }
+  backend "http" {
+    address        = "https://api.tfstate.dev/github/v1"
+    lock_address   = "https://api.tfstate.dev/github/v1/lock"
+    unlock_address = "https://api.tfstate.dev/github/v1/lock"
+    lock_method    = "PUT"
+    unlock_method  = "DELETE"
+    username       = "dotemacs/tofuplay"
   }
 }
 
-resource "local_file""hello_local_file" {
-  content = "Hello terraform local!"
-  filename = "${path.module}/hello_local_file.txt"
-}
+  resource "local_file""hello_local_file" {
+    content = "Hello terraform local!"
+    filename = "${path.module}/hello_local_file.txt"
+  }
