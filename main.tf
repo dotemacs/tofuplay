a391376d19e98fbb7ed95e0d9959542fef6329cc
Александар Симић <a@repl.ist>
2024-04-04 10:41:00 +0100

    Prevent the fallback in case the env var can't be read

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  encryption {
+    state {
+      enforced = true
+    }
+    plan {
+      enforced = true
+    }
+  }
+}
+
 resource "local_file""hello_local_file" {
   content = "Hello terraform local!"
   filename = "${path.module}/hello_local_file.txt"
